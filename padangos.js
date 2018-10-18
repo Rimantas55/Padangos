c0e5ed01b84880628d04cbbb42aa8661e5351928
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2018-10-18 18:10:35 +0300

    funkcija ar telpa ratas uzbaigta liko..
    pameginau pakurti funkcija del kilomettru ir ratu apsisukimu skaiciaus, dar neintegravau i bendra funkcija, nesuprantu kodel man neranda var = distance
    atsakymas  .js :19 / norejau sudeti visa atsakyma i viena div ir prideti "<br />" bet kazkodel nerado.

--- padangos.js
+++ padangos.js
@@ -1,57 +1,52 @@
 console.log('padangos working')
 
-// Reiktų vengti lietuviškų kintamųjų pavadinimų
+// Reiktų vengti lietuviškų kintamųjų pavadinimų - ATS: kitam projektui bandysiu naudoti tik EN
 var input_padang_aukstis = document.getElementById('padangos_aukstis');
 var input_ratl_skersmuo = document.getElementById('ratlankio_skersmuo');
 var input_arka_spindulys = document.getElementById('arkos_spindylys');
 var div_skaiciuoti_rez = document.getElementById('rezultatas');
-var laisvas_tarpas = 10; // cm? mm? ir niekur nepanaudota
+var laisvas_tarpas = 10; // Rimantas - kazkaip niekir nepanaudojau, gal 23 eiluteje galima buvo vietoj 10?
+var input_km = document.getElementById('kilometras'); //kilomteru inputas
 
 function skaiciuoti() {
     var a = parseInt(input_padang_aukstis.value) / 10;    /*--convert mm--> CM--*/
-    var b = parseInt(input_ratl_skersmuo.value) * 25.4;    /*--convert inch--> CM--*/
+    var b = parseInt(input_ratl_skersmuo.value) * 2.54;    /*--convert inch--> CM--*/
     var c = parseInt(input_arka_spindulys.value);
 
     // kintamojo pavadinimas neatspindi kas jame saugoma.. gal verčiau "wheel_radius" ar "full_wheel_diameter"
     var skaiciuoti = Math.round(a + b + c);
 
-    div_skaiciuoti_rez.innerHTML = skaiciuoti + " cm ";
+    div_skaiciuoti_rez.innerHTML = skaiciuoti + " cm " + " Padangos1 apsisukimu skaicius nuvaziavus " + distance + " yra " + total_wheel_rounds;
 
-    console.log('Skaičiavimo rezultatas: ' + skaiciuoti);
+	console.log('Skaičiavimo rezultatas: ' + skaiciuoti);
 
-
+    if (a + b < c - 10){ //arba var laisvas_tarpas = 10?;
+    console.log('ratas telpa i arka');
+    document.getElementById('rezultatas').style.background = 'lightgreen';
+    document.getElementById('rezultatas').innerHTML = "Ratas telpa i arka!";
+    document.getElementById('rezultatas').style.color = "white";
+	document.getElementById('rezultatas').style.fontSize = "200%";
+	
+    } else {
+    document.getElementById('rezultatas').style.background = 'red';
+    document.getElementById('rezultatas').innerHTML = "Ratas netelpa i arka!";
+    document.getElementById('rezultatas').style.color = "white";
+	document.getElementById('rezultatas').style.fontSize = "200%";
+	
+    }
+	
 }
 
 
-// if (a >= 50 || a <= 300   ){
-// 	alert("Number A or Number B is above 100");
-// 	input_a.style.color = 'red';
-// 	input_b.style.color = 'red';
-// } else {
+  //kiek apsisuks ratas nuvaziavus 100 km? - kolkas dar neadaptavau prie rezultato skaiciavimo*/
+  function wheel_rounds_per_km(){
+	  var a = parseInt(input_padang_aukstis.value) / 10;    //--convert mm--> CM--
+	  var b = parseInt(input_ratl_skersmuo.value) * 2.54;    //--convert inch--> CM--
+	  var distance = parseInt(input_km.value); //value yra kilomterais
+	  var wheel_length = 
+	  ((a + b) * 3.14) / 100000;//verciam i km (global input variable atrodo visasi nereikalingi
+	  var wheel_round_one_time = wheel_length;
+	  var total_wheel_rounds = wheel_length * distance; 
 
-// 	var sum = a + b;
-// console.log(sum);
+}
 
-// 		var eur = convert_to_eur(sum);
-// 		result.innerHTML = sum + ' (Eur: ' + eur +")";
-// 		input_a.style.background = "green";
-// 		input_b.style.background = "green";
-// 	}
-// }
-
-
-// if (a > 100 || b > 100){
-// 		alert("Number A or Number B is above 100");
-// 		input_a.style.color = 'red';
-// 		input_b.style.color = 'red';
-// 	} else {
-
-// 		var sum = a + b;
-// 		// console.log(sum);
-
-// 		var eur = convert_to_eur(sum);
-// 		result.innerHTML = sum + ' (Eur: ' + eur +")";
-// 		input_a.style.background = "green";
-// 		input_b.style.background = "green";
-// 	}
-// }

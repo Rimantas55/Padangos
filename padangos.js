3750c293e8ab5d1fe124ede10a6720da37b9e9f2
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2018-10-20 00:55:19 +0300

    Skaiciuoja rato apsisukimus
    kazkoks keistas apsisukusiu ratu gautas rezultatas.

--- padangos.js
+++ padangos.js
@@ -5,55 +5,38 @@ var input_padang_aukstis = document.getElementById('padangos_aukstis');
 var input_ratl_skersmuo = document.getElementById('ratlankio_skersmuo');
 var input_arka_spindulys = document.getElementById('arkos_spindylys');
 var div_skaiciuoti_rez = document.getElementById('rezultatas');
-var laisvas_tarpas = 10; // Rimantas - kazkaip niekir nepanaudojau, gal 23 eiluteje galima buvo vietoj 10?
-var input_km = document.getElementById('kilometras'); //kilomteru inputas
-var distance = parseInt(input_km.value);
+var laisvas_tarpas = 10; 
+var input_km = document.getElementById('kilometras'); 	 //kilomteru inputas
+
 
 function skaiciuoti() {
+    /*--rato ir arkos funkcijos kintamieji--*/
     var a = parseInt(input_padang_aukstis.value) / 10;    /*--convert mm--> CM--*/
-    var b = parseInt(input_ratl_skersmuo.value) * 2.54;    /*--convert inch--> CM--*/
+    var b = parseInt(input_ratl_skersmuo.value) * 2.54;   /*--convert inch--> CM--*/
     var c = parseInt(input_arka_spindulys.value);
-
-    // kintamojo pavadinimas neatspindi kas jame saugoma.. gal verčiau "wheel_radius" ar "full_wheel_diameter"
     var radius = Math.round(a + b/2 + laisvas_tarpas);
-
+    /*--Bonuso kintamieji rato apsisukimo kartai nurodzius kilometrus--*/
+    var wheel_length = Math.round((a + b) * 3.14 * 10**5);  		  //verciam i km (global input variable atrodo visasi nereikalingi
+    var distance = parseInt(input_km.value);
+	var total_wheel_rounds = wheel_length * distance; 
+    
     div_skaiciuoti_rez.innerHTML = radius + " cm ";
+    console.log('Skaičiavimo rezultatas: ' + radius);
 
-    //bonuso papildomas atsakymas
-    // + " Padangos1 apsisukimu skaicius nuvaziavus " + 
-    // distance + " yra " + total_wheel_rounds;
-
-	console.log('Skaičiavimo rezultatas: ' + radius);
-
-    if (c > radius ){ //arba var laisvas_tarpas = 10?;
+    if (c > radius ){
     console.log('ratas telpa i arka');
     document.getElementById('rezultatas').style.background = 'lightgreen';
-    document.getElementById('rezultatas').innerHTML = "Ratas telpa i arka!";
+    document.getElementById('rezultatas').innerHTML = "Ratas telpa i arka!" + "<br>" + "Nuvaziavus " + distance + " km ratas apsisuks " + total_wheel_rounds + " kartus";
     document.getElementById('rezultatas').style.color = "white";
-	document.getElementById('rezultatas').style.fontSize = "200%";
-
-	
+	document.getElementById('rezultatas').style.fontSize = "150%";
     } else {
     document.getElementById('rezultatas').style.background = 'red';
     document.getElementById('rezultatas').innerHTML = "Ratas netelpa i arka!";
     document.getElementById('rezultatas').style.color = "white";
 	document.getElementById('rezultatas').style.fontSize = "200%";
-	
     }
-
-	
 }
 
 
-  //kiek apsisuks ratas nuvaziavus 100 km? - kolkas dar neadaptavau prie rezultato skaiciavimo*/
-//  function skaiciuoti(){
-// 	  var a = parseInt(input_padang_aukstis.value) / 10;    //--convert mm--> CM--
-// 	  var b = parseInt(input_ratl_skersmuo.value) * 2.54;    //--convert inch--> CM--
-// 	  var distance = parseInt(input_km.value); //value yra kilomterais
-// 	  var wheel_length = 
-// 	  ((a + b) * 3.14) / 100000;//verciam i km (global input variable atrodo visasi nereikalingi
-// 	  var wheel_round_one_time = wheel_length;
-// 	  var total_wheel_rounds = wheel_length * distance; 
-
-// }
+  
 	
